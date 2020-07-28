2f1b33a4db6a860b5f04dfbf48496834d31ec927
yapie <targaryen.akane@gmail.com>
2020-07-29 00:46:18 +0700

    update sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'pwgen-cache-v1';
+var urlsToCache = [
+  './?v3',
+  'icon.png',
+  'index.html',
+  'main.js',
+  'manifest.json',
+  'sw.js',
+  'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
+  'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js',
+];
+console.log('loading sw');
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  console.log('installing sw');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      var x = cache.addAll(urlsToCache);
+      console.log('cache added');
+      return x;
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
