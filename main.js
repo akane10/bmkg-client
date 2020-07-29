45dc659dd087469d4fcc08a5a499930002970515
yapie <targaryen.akane@gmail.com>
2020-07-30 00:09:17 +0700

    a lot

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ const gempaterkini = document.getElementById('gempaterkini');
 const gempadirasakan = document.getElementById('gempadirasakan');
 const lasttsunami = document.getElementById('lasttsunami');
 
+const compose = (...functions) => (args) =>
+  functions.reduceRight((arg, fn) => fn(arg), args);
+
+const trace = (label) => (value) => {
+  console.log(`${label}: ${value}`);
+  return value;
+};
+
+const render = (id) => (html) => (id.innerHTML += html);
+const take = (num) => (arr) => arr.slice(0, num);
+const join = (str) => (arr) => arr.join(str);
+const addPadding = (arr) =>
+  arr.map((i) => `<div style="padding-bottom:20px">${i}</div>`);
+const takeTwo = take(2);
+const afterTakeTwo = trace('after take 2');
+const beforeTakeTwo = trace('before take 2');
+
 const BASEURL = 'https://gempa.yapie.me/api/gempa';
 
 async function getData(path) {
@@ -19,22 +36,48 @@ async function getData(path) {
     console.error(e);
   }
 }
-const objToString = (obj) =>
+
+const objToHtmlString = (obj) =>
   Object.entries(obj).map(
-    ([key, value]) => `<div class="has-text-centered"> ${key}: ${value} </div>`
+    ([key, value]) => `
+    <div class="has-text-centered"> 
+      <div class="title is-size-6" style="margin-bottom:0">
+        ${key}
+      </div>
+      <div>
+        ${value} 
+      </div>
+    </div>
+    `
   );
+
+const toObject = (acc, [key, value]) => {
+  acc[key] = value;
+  acc;
+};
+
+const setKeys = (obj) => {
+  const keys = ['Tanggal', 'Jam', 'Magnitude', 'Potensi', 'Area', 'Wilayah'];
+
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    if (keys.includes(key) || key.includes('Wilayah')) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+};
 
 async function gempa(path) {
   const { data } = await getData(path);
 
-  return data.map((i) => {
-    return objToString(i).join('');
-  });
+  return data.map(setKeys).map(compose(join(''), objToHtmlString));
 }
 
-const render = (id) => (html) => (id.innerHTML += html);
-
-gempa('autogempa').then(render(autogempa));
-gempa('gempaterkini').then(render(gempaterkini));
-gempa('gempadirasakan').then(render(gempadirasakan));
-gempa('lasttsunami').then(render(lasttsunami));
+gempa('autogempa').then(compose(render(autogempa), addPadding));
+gempa('gempaterkini').then(
+  compose(render(gempaterkini), join(''), addPadding, takeTwo)
+);
+gempa('gempadirasakan').then(
+  compose(render(gempadirasakan), join(''), addPadding, takeTwo)
+);
+gempa('lasttsunami').then(compose(render(lasttsunami), addPadding));
