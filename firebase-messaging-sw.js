0f91947a28a6168d89358d3c7517b765b636d028
yapie <targaryen.akane@gmail.com>
2020-07-30 23:35:42 +0700

    seems ok

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,3 +8,43 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registration failed, error:', err);
     });
 }
+
+self.addEventListener('notificationclick', function (e) {
+  var notification = e.notification;
+  // var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('http://www.example.com');
+    notification.close();
+  }
+});
+
+self.addEventListener('notificationclose', function (e) {
+  var notification = e.notification;
+  // var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ');
+});
+
+self.addEventListener('push', function (event) {
+  console.log('Received a push message', event);
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const { notification } = event.data.json();
+
+  var title = notification.title || 'Yay a message.';
+  var body = notification.body || 'We have received a push message.';
+  var icon = '/icon.png';
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag,
+    })
+  );
+});
